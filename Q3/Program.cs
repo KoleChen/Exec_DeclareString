d80c1fc97e710a4383da48de9295293650939713
User <User@DESKTOP-M776RP5>
2022-10-16 19:36:35 +0800

    加入專案檔案。

---
+++ Q3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("我是第一行\n我是第二行");
+        }
+    }
+}
